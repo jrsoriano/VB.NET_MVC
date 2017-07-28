9e892c795e96cb24ec197336d27425bd6b7a0cd1
Jose R. Soriano <jrsoriano@users.noreply.github.com>
2017-07-27 23:14:17 -0500

    Update Course.vb

--- Course.vb
+++ Course.vb
@@ -7,17 +7,22 @@ Namespace Models
 	<Bind(Exclude: = "CourseId")>
 		
 		Public Class Course
+			
 			<ScaffoldColumn(False)>
 			Public Property CouseId As Integer
+			
 			<DisplayName("Discipline")>
 			Public Property DiciplineId As Integer
+			
 			<DisplayName("Professor")>
 			Public Property ProfessorId As Integer
+			
 			<Required(ErrorMessage:= "A Course Name is required")>
 			<StringLength(160)>
 			Public Property CourseName As String
+			
 			<Required(ErrorMessage:= "Fee is required")>
-			<Range(0.01, 100, ErrorMessage:= "Price must be between 0.01 and 100.00")>
+			<Range(0.01, 100, ErrorMessage:= "Fee charged must be between 0.01 and 100.00")>
 			Public Property Fee as Decimal
 		
 			Public Overridable Property Dicipline AS Dicipline
